3501fa0fe7a6586b0d1b9e1a72b06a7b583d82da
Aaditya Bansal <aadityabansal522@gmail.com>
2026-03-26 14:47:08 +0530

    updated cors for render deployment

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://contact-frontend-afvm.onrender.com"
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.error("MongoDB Error:", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Welcome user, server is running");
@@ -19,5 +31,6 @@ app.get("/", (req, res) => {
 
 app.use('/api/contacts', require('./routes/contactRoutes'));
 
-
-app.listen(PORT, () => {console.log(`Server running on port ${PORT}`);});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
